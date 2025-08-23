0fe737133ad67a06fa463e5bc5f0e0463091a9ec
haklesh051 <hakleshbairwa051@gmail.com>
2025-08-23 14:41:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+set -e
+
+echo "Cloning Repo..."
+git clone https://github.com/Popeye68/TXT-EXTRACTOR /app/TXT-EXTRACTOR
+
+cd /app/TXT-EXTRACTOR
+
+echo "Installing requirements..."
+pip install --no-cache-dir -r requirements.txt
+
 echo "Starting Bot..."
-python -m Extractor
+exec python .
