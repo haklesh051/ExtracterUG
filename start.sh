30fb6e70735920ea3b30ae929273cb0f187bbc8f
haklesh051 <hakleshbairwa051@gmail.com>
2025-08-23 14:49:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Cloning Repo..."
-git clone https://github.com/Popeye68/TXT-EXTRACTOR /app/TXT-EXTRACTOR
-
-cd /app/TXT-EXTRACTOR
-
 echo "Installing requirements..."
 pip install --no-cache-dir -r requirements.txt
 
 echo "Starting Bot..."
-exec python .
+python -m Extractor
+# OR agar main.py se run karna ho:
+# python main.py
